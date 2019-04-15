77535c80022cc99b89207217e89a6b48bbdedc84
Antsiferov Maxim <antsiferovmaximv@gmail.com>
2019-04-15 13:11:10 +0300

    Add now.sh

--- src/constants/api.constants.js
+++ src/constants/api.constants.js
@@ -1,5 +1,5 @@
 const devProtocol = 'http://';
-const devHost = '192.168.8.100';
+const devHost = '192.168.0.101';
 const devPort = ':3010';
 const devPrefix = '/api';
 const devBaseUrl = devProtocol + devHost + devPort + devPrefix;
