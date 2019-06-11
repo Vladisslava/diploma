2c4232f548a3125b3dc9a39e503360c9aacf00f0
AntsiferovMaxim <antsiferovmaximv@gmail.com>
2019-06-11 16:06:46 +0300

    Fix

--- src/constants/api.constants.js
+++ src/constants/api.constants.js
@@ -1,17 +1,17 @@
-const devProtocol = 'http://';
+/*const devProtocol = 'http://';
 const devHost = '192.168.8.101'; // https://vladisslava-diploma-backend.herokuapp.com/
 const devPort = ':8080';
 const devPrefix = '/api';
 const devBaseUrl = devProtocol + devHost + devPort + devPrefix;
-const staticHost = devProtocol + devHost + devPort;
+const staticHost = devProtocol + devHost + devPort;*/
 
-/*const devProtocol = 'https://';
+const devProtocol = 'https://';
 // const devHost = '31.131.28.162'; // https://vladisslava-diploma-backend.herokuapp.com/
 const devHost = 'vladisslava-diploma-backend.herokuapp.com'; // https://vladisslava-diploma-backend.herokuapp.com/
 const devPort = '';
 const devPrefix = '/api';
 const devBaseUrl = devProtocol + devHost + devPort + devPrefix;
-const staticHost = devProtocol + devHost + devPort;*/
+const staticHost = devProtocol + devHost + devPort;
 
 const apiConstants = {
     baseUrl: devBaseUrl,
